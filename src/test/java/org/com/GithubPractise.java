537e7db31eda1ddc3c030405deb82de5e0ad3f9d
Dinesh-181 <dineshthala181@gmail.com>
2023-09-20 15:23:47 +0530

    master

---
+++ src/test/java/org/com/GithubPractise.java
@@ -0,0 +1,20 @@
+package org.com;
+
+public class GithubPractise {
+	 
+			public static void main(String[]args)
+			{
+			int[] a = new int[]{ 1,2,3,4,5,6,7,8,9,34};
+			int sum=0 ;
+			{
+				for(int i =0;i<a.length;i++)
+		            sum =sum + a[i] ;  	}
+			System.out.println("sum of the all elment of an a:" +sum);
+				
+			}
+			
+			
+
+		}
+
+
